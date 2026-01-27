96e0e79494ccf35fb13e3f430064ac6bb99ad110
gayatri <gayatrias1608@gmail.com>
2026-01-27 23:01:06 +0530

    created auth table using alembic

---
+++ src/auth/models.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime
+
+class UserAuthModel(SQLModel, table=True):
+    __tablename__ = "authtable"
+
+    id : int | None = Field(default=None, primary_key=True)
+    username : str
+    email : str
+    password : str
+    created_at : datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at : datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
